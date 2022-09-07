4b5daa01667cf59b369a5dcc0ce184ef8f79ef8c
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-07 13:58:22 -0400

    types created

---
+++ src/context/types.js
@@ -0,0 +1,10 @@
+// darkMode
+export const SWITCH_DARKMODE = 'SWITCH_DARKMODE';
+
+// user management
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+// fill files list
+export const SET_FILES = 'SET_FILES';
+export const UPDATE_FILES = 'UPDATE_FILES';

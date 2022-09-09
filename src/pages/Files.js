ab0a584d1da799c5cdd00db1db2e09a6bb4093b5
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-09 14:17:54 -0400

    loadFiles added

--- src/pages/Files.js
+++ src/pages/Files.js
@@ -1,12 +1,37 @@
-import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 
 import AppContext from '../context/AppContext';
 
 function Files() {
   const { user, files, setFiles } = useContext(AppContext);
   const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {}, []);
+  const loadFiles = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `${process.env.REACT_APP_SERVER_HOST}file/files`,
+        { headers: { Authorization: `Bearer ${user.token}` } }
+      );
+      setFiles(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      if (error.response) {
+        error.response.data ? setErrors(error.response.data) : setErrors({});
+      } else if (error.request) {
+        setErrors({ general: 'No response received' });
+      } else {
+        setErrors({ general: error.message });
+      }
+    }
+  }, [user.token, setFiles]);
+
+  useEffect(() => {
+    loadFiles();
+  }, [loadFiles]);
 
   return <div>Files</div>;
 }

0c22fa0a29be097233508145ff763611de217115
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-09 14:56:47 -0400

    details added

--- src/components/FileDetails.js
+++ src/components/FileDetails.js
@@ -1,10 +1,55 @@
+import { Card, Stack, Typography } from '@mui/material';
 import React from 'react';
 
 function FileDetails({ file }) {
   const { id, name, originalFilename, size, status, updatedAt, createdAt } =
     file;
-  console.log(name);
-  return <div>FileDetails</div>;
+  return (
+    <Card style={{ width: '80%' }}>
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={1}
+        padding={1}
+        style={{ width: '100%' }}
+      >
+        <Stack
+          direction="row"
+          justifyContent="start"
+          alignItems="center"
+          spacing={1}
+          padding={1}
+          style={{ width: '100%' }}
+        >
+          <Typography variant="h5">Name:</Typography>
+          <Typography variant="h5">{name}</Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="start"
+          alignItems="center"
+          spacing={1}
+          padding={1}
+          style={{ width: '100%' }}
+        >
+          <Typography variant="caption">Original name:</Typography>
+          <Typography variant="caption">{originalFilename}</Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="start"
+          alignItems="center"
+          spacing={1}
+          padding={1}
+          style={{ width: '100%' }}
+        >
+          <Typography variant="caption">Size:</Typography>
+          <Typography variant="caption">{size} bytes</Typography>
+        </Stack>
+      </Stack>
+    </Card>
+  );
 }
 
 export default FileDetails;
